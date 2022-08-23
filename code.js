0bfb327b4a49ac976d5b45849ebd5fd82052c815
DubanAlbarracinI <dalbarracini@ucentral.edu.co>
2022-08-22 21:06:47 -0500

    Taller logica

--- code.js
+++ code.js
@@ -3,114 +3,121 @@ function sumatoria_ejercicio1() {
     let operacion_total = 0
     for (let i = 1; i <= sumatoria_total; i++) {
         operacion_total = operacion_total + i
-        document.querySelector("#total_sumatoria_resultado").innerHTML += `
+    }
+    document.querySelector("#total_sumatoria_resultado").innerHTML = `
             La sumatoria total es ${operacion_total}
         `
-    }
 }
 
-function pizzeria_pepitos() {
-    let edad = document.querySelector("#edad_pizzeria").value
-    let sexo = document.querySelector("#sexo_pizzeria").value
-
-    let premios = ['jugo', 'cerveza', 'pizza tres carnes', 'pizza hawaina']
-
-    if (sexo == 'hombre') {
-        if (edad <= 10) {
-            // document.querySelector("#resultado_pizzeria").innerHTML = `
-            //     Tiene derecho a reclamar ${premios[0]}
-            // `
-            Swal.fire({
-                title: `Ganaste un ${premios[0]}`,
-                width: 600,
-                padding: '3em',
-                color: '#716add',
-                background: '#fff url(assets/img/trees.png)',
-                backdrop: `
-                    rgba(0,0,123,0.4)
-                    url("assets/img/nyan-cat.gif")
-                    left top
-                    no-repeat
-                `
-            })
-        } else if (edad >= 18) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Ganaste',
-                text: `Tienes derecho a una ${premios[1]} y una ${premios[2]}`
-            })
-        }
-    } else if (sexo == 'mujer') {
-        if (edad >= 18) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Ganaste',
-                text: `Tienes derecho a una ${premios[1]} y una ${premios[3]}`
-            })
-        }
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Sera para una proxima vez!!',
-        })
-    }
-
-    // if(sexo == 'hombre' && edad <= 18){
-
-    // }else if(sexo == 'mujer' && edad >= 18){
-
-    // }
-
-}
-
-
-localStorage.setItem("pizza", 3000)
-localStorage.setItem("gaseosa", 9000)
-localStorage.setItem("hamburguesas", 15000)
-localStorage.setItem("perros", 5000)
-
-
-function solicitar_comida() {
-    let comida_solicitada = document.querySelector("#pedido_comida").value
-    if (comida_solicitada != 'pagar') {
-        if (localStorage.getItem(comida_solicitada) != null) {
-            let precio_comida = parseInt(localStorage.getItem(comida_solicitada))
-            let total_a_pagar = 0
-            if (localStorage.getItem("precio_pago") == null) {
-                total_a_pagar = total_a_pagar + precio_comida
-                localStorage.setItem("precio_pago", total_a_pagar)
-            } else {
-                total_a_pagar = parseInt(localStorage.getItem("precio_pago"))
-                total_a_pagar = total_a_pagar + precio_comida
-                localStorage.setItem("precio_pago", total_a_pagar)
-            }
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'No existe el plato solicitado',
-                text: 'Digite algo bien... burro',
-            })
-            document.querySelector("#pedido_comida").value = ''
-        }
-    } else {
-        let precio_factura = localStorage.getItem("precio_pago")
-        Swal.fire({
-            icon: 'info',
-            title: `El precio a pagar es $${precio_factura}`,
-        })
-        localStorage.removeItem("precio_pago")
-    }
-}
-
-function generar_tabla() {
-    let inicio = document.querySelector("#numero_tabla").value
-    let limite = document.querySelector("#limite_tabla").value
-    let multiplicacion = 0
-    for (let x = 1; x <= limite; x++) {
-        multiplicacion = x * inicio
-        document.querySelector("#resultado_tabla").innerHTML += `
-            <h5>${inicio} X ${x} = ${multiplicacion}</h5>
+function ejercicio2() {
+    let numero = document.querySelector("#numero_ejercicio2").value
+    if (numero % 2 == 0) {
+        document.querySelector("#resultado2").innerHTML = `
+            El numero ${numero} es par
         `
-
+    } else {
+        document.querySelector("#resultado2").innerHTML = `
+            El numero ${numero} es impar
+        `
     }
+
+}
+
+function ejercicio3() {
+    let edad = document.querySelector("#numero_ejercicio3").value
+    let sexo = document.querySelector("#numero_ejercicio3_2").value
+    console.log(edad)
+    if (edad <= 10 && edad > 0) {
+        Swal.fire(
+            'Ganaste?',
+            'Reclama un jugo',
+            'success'
+        )
+    } else if (edad >= 18) {
+        Swal.fire(
+            'Ganaste?',
+            'Reclama una cerveza',
+            'success'
+        )
+        if (sexo == "M") {
+            Swal.fire(
+                'Ganaste?',
+                'Reclama una cerveza y una porcion de pizza Hawaiana',
+                'success'
+            )
+        }
+        if (sexo == "H") {
+            Swal.fire(
+                'Ganaste?',
+                'Reclama una cerveza y una porcion de pizza de tres carnes',
+                'success'
+            )
+        }
+    } else {
+        Swal.fire(
+            'Perdiste?',
+            'No puedes reclamar ningun premio',
+            'error'
+        )
+    }
+
+}
+
+let total = 0
+let menu = [
+    {
+        nombre: "coca-cola",
+        precio: 3500
+    },
+    {
+        nombre: "postobon",
+        precio: 1500
+    },
+    {
+        nombre: "fanta",
+        precio: 2000
+    },
+    {
+        nombre: "jugo hit",
+        precio: 2500
+    },
+    {
+        nombre: "pepsi",
+        precio: 1700
+    }
+]
+function ejercicio4() {
+    let palabra = document.querySelector("#numero_ejercicio4").value
+
+    if (palabra == "pagar") {
+
+        document.querySelector("#resultado4").innerHTML = `
+            total a pagar ${total} 
+        `
+        total = 0
+    } else {
+        menu.forEach(key => {
+            if (palabra == key.nombre) {
+                total += key.precio
+            }
+        })
+    }
+}
+
+let total_suma = 0
+let multi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function ejercicio5() {
+    let numero = document.querySelector("#numero_ejercicio5").value
+    document.querySelector("#resultado5").innerHTML = ``
+    multi.forEach(element => {
+        let res = numero * element
+        total_suma += res
+        document.querySelector("#resultado5").innerHTML += `
+            ${numero} x ${element} = ${res} <br>
+        `
+    });
+    document.querySelector("#resultado5").innerHTML += `
+            total es ${total_suma} 
+        `
+    total_suma = 0
 }
